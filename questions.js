b72d111f6a8f2c69f5621e4a4663c2603b7c6fd2
Jorge Victor Ferreira de Lima <100483548+AlpharJorge@users.noreply.github.com>
2023-08-09 23:49:08 -0300

    Criando perguntas

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Para que serve a tag head?",
+        answer: "B. Adicionar elementos na cabeça do site",
+        options: [
+            "A. Nomear um website",
+            "B. Adicionar elementos na cabeça do site",
+            "C. Definir idioma do site",
+            "D. Título do site"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Para que serve a tag body?",
+        answer: "C. Adicionar elementos no corpo do site",
+        options: [
+         "A. Escrever textos",
+         "B. Definir tamanho do website",
+         "C. Adicionar elementos no corpo do site",
+         "D. Definir cabeçalho"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quais as outras linguagens de programação o HTML deixa utilizar junto com ela?",
+        answer: "C. CSS e JavaScript",
+        options: [
+         "A. PHP e Dark",
+         "B. PHP e CSS",
+         "C. CSS e JavaScript",
+         "D. CSS 3, JavaScript e XML"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Por que colocamos barra nas tags?",
+        answer: "A. Para fechar a tag",
+        options: [
+          "A. Para fechar a tag",
+          "B. Para começar a tag",
+          "C. Para quebra de linha",
+          "D. Para estilizar"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual a função da tag <br>?",
+        answer: "D. Quebrar linha",
+        options: [
+          "A. Sublinhar conteudo",
+          "B. Recarregar pagina",
+          "C. Aumentar tamanho da fonte",
+          "D. Quebrar linha"
+        ]
+    },
+];
